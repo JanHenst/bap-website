4d2f94b19eca7c16b39a4a2003fa5013ad935411
Jan van der Henst <26615@ma-web.nl>
2020-01-22 16:22:12 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,12 @@
 
 Route::get('/', 'HomeController@index')->name('homepage');
 
-
-Route::get('/producten', 'ProductController@index')->name('product.list');
-Route::get('/producten/{id}', 'ProductController@details')->name('product.details');
-
 Route::prefix('/admin')->middleware('auth')->group(function() {
-  Route::get('/producten/add', 'ProductController@create')->name('add_products');
-  Route::post('/producten/add', 'ProductController@store')->name('store_products');
   Route::get('/blogs/add', 'BlogController@create')->name('add_blogs');
   Route::post('/blogs/add', 'BlogController@store')->name('store_blogs');
 });
 
-
 Route::get('/blogs', 'BlogController@index')->name('blog.list');
 Route::get('/blogs/{id}', 'BlogController@details')->name('blog.details');
-
 
 Auth::routes();
